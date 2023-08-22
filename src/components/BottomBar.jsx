60c41d2638bb483d9bd36a2c24386b4b3dd73036
msjt13 <msjt13@gmail.com>
2023-08-23 01:20:34 +0600

    initial

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import Link from "next/link";
 
 const BottomBar = () => {
     return (
         <main>
-            <h1>Bottom Bar</h1>
+            <footer className="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+                <div className="max-w-screen-xl mx-auto text-center">
+                    <Link
+                        href="#"
+                        className="flex items-center justify-center text-2xl font-semibold text-gray-900 dark:text-white"
+                    >
+                        STB
+                    </Link>
+                    <p className="my-6 text-gray-500 dark:text-gray-400">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, dolorum.
+                    </p>
+                    <ul className="flex flex-wrap items-center justify-center mb-6 text-gray-900 dark:text-white">
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6 ">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6">
+                                Premium
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6 ">
+                                Campaigns
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6">
+                                Blog
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6">
+                                Affiliate Program
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6">
+                                FAQs
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    © 2021-2023{' '}
+                        <Link href="#" className="hover:underline">
+                        STB
+                    </Link>
+                    . All Rights Reserved.
+                </span>
+                </div>
+            </footer>
         </main>
     );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,64 @@ const NavBar = () => {
     return (
         <main>
             <header>
-                <nav className="py-2.5 bg-white border-gray-200 lg:px-6 dark:bg-gray-800">
+                <nav className="py-2.5 bg-white border-gray-200 px-6 dark:bg-gray-800">
                     <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
                         <Link href="/" className="flex items-center">
                             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                                 STB
                             </span>
                         </Link>
+                        <div className="flex items-center order-2">
+                            <Link
+                                href="#"
+                                className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                            >
+                                Log in
+                            </Link>
+                            <Link
+                                href="#"
+                                className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                            >
+                                Get started
+                            </Link>
+                        </div>
+                        <div className="items-center justify-between flex w-auto order-1">
+                            <ul className="flex font-medium flex-row space-x-8 mt-0">
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="block py-2 pl-3 pr-4 text-white rounded bg-primary-700 bg-transparent text-primary-700 p-0 dark:text-white"
+                                        aria-current="page"
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 hover:bg-transparent border-0 hover:text-primary-700 p-0 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700"
+                                    >
+                                        About
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 hover:bg-transparent border-0 hover:text-primary-700 p-0 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700"
+                                    >
+                                        Blog
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 hover:bg-transparent border-0 hover:text-primary-700 p-0 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700"
+                                    >
+                                        Contact
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </header>
